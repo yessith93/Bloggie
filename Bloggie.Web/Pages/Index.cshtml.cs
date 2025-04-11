639313a5b29d09468402112d3065a02ba2790835
yessith93 <almanza.m@outlook.com>
2025-04-10 20:11:10 -0500

    [home page done]

--- Bloggie.Web/Pages/Index.cshtml.cs
+++ Bloggie.Web/Pages/Index.cshtml.cs
@@ -1,20 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Bloggie.Web.Repositories;
+using Bloggie.Web.Models.Domain;
 
 namespace Bloggie.Web.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IBlogPostRepository _blogPostRepository;
+        public List<BlogPost> Blogs { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IBlogPostRepository BlogPostRepository)
         {
             _logger = logger;
+            _blogPostRepository = BlogPostRepository;
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGet()
         {
-
+            Blogs = (await _blogPostRepository.GetAllBlogPostsAsync()).ToList();
+            return Page();
         }
     }
 }
