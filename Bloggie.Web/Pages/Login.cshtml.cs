514a40704fd9b341a0712a736089768dca7959c3
yessith93 <almanza.m@outlook.com>
2025-04-24 21:27:32 -0500

    Login page added

---
+++ Bloggie.Web/Pages/Login.cshtml.cs
@@ -0,0 +1,41 @@
+using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bloggie.Web.Pages
+{
+    public class LoginModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        [BindProperty]
+    public Login LoginViewModel { get; set; }
+        public LoginModel(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+        public void OnGet()
+        {
+        }
+        public async Task<IActionResult> OnPost(string url) 
+        {
+            var signInResult = await _signInManager.PasswordSignInAsync(LoginViewModel.Username, LoginViewModel.Password, isPersistent: false, lockoutOnFailure: false);
+
+            if (signInResult.Succeeded)
+            {
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                ViewData["Notification"] = new Notification
+                {
+                    Type = Enums.NotificationType.Error,
+                    Message = "Unable to login"
+                };
+
+                return Page();
+            }
+        }
+    }
+}
