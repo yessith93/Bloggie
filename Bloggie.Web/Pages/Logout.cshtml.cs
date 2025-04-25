71c094c12fd3d04a60d714091a84f005ac0540b1
yessith93 <almanza.m@outlook.com>
2025-04-24 21:47:01 -0500

    logout page

---
+++ Bloggie.Web/Pages/Logout.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bloggie.Web.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public LogoutModel(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+        public async Task<IActionResult> OnGet()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToPage("Index");
+        }
+    }
+}
