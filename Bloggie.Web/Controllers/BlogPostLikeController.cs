bf8e593132534d8b00750de69c6223ba5feb2e8b
yessith93 <almanza.m@outlook.com>
2025-05-01 21:55:51 -0500

    Add BlogPostLike controller and view model; update blog details to include like button

---
+++ Bloggie.Web/Controllers/BlogPostLikeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class BlogPostLikeController : Controller
+    {
+        [Route("Add")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
